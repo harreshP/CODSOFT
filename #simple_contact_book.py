ba98a54990e02b9bf0d13fec707ee62fc44cc2c6
harreshP <btechece230999@smvec.ac.in>
2025-06-24 23:55:13 +0530

    simple_contact_book.py

--- #simple_contact_book.py
+++ #simple_contact_book.py
@@ -1,50 +1,50 @@
 contacts={}
 def add_contact():
-  name=input("Enter name:")
-  phone=inpu("Enter phone number:")
-  contacts[name]=phone
-  print("Contact added!\n")
+    name=input("Enter name:")
+    phone=input("Enter phone number:")
+    contacts[name]=phone
+    print("Contact added!\n")
 def view_contacts():
-  if not contacts:
-    print("no contacts saved.\n")
-  else:
-    print("\ncontact List:")
-    for name,phone in contacts.items():
-      print(f"{name}:{phone}")
-      print()
+    if not contacts:
+        print("no contacts saved.\n")
+    else:
+        print("\ncontact List:")
+        for name,phone in contacts.items():
+            print(f"{name}:{phone}")
+        print()
 def search_contacts():
-  name=inputs("Enter name to search:")
-  if name in contacts:
-    print(f"{name}'s number is {contacts[name]}\n")
-  else:
-    print("contact not found.\n")
+    name=input("Enter name to search:")
+    if name in contacts:
+        print(f"{name}'s number is {contacts[name]}\n")
+    else:
+        print("contact not found.\n")
 def delete_contact():
-  name=inout("Enter name to delete:")
-  if name in contacts:
-    del contacts[name]
-    print("contact deleted.\n")
-  else:
-    print("contact not found.\n")
+    name=input("Enter name to delete:")
+    if name in contacts:
+        del contacts[name]
+        print("contact deleted.\n")
+    else:
+        print("contact not found.\n")
 def menu():
-  while true:
-    print("==contact book===')
-          print("1.Add contact")
-          print("2.view contact")
-          print("3.search contact")
-          print("4.delete contact")
-          print("5.exit")
+    while True:
+        print("=== Contact book ===")
+        print("1.Add contact")
+        print("2.view contact")
+        print("3.search contact")
+        print("4.delete contact")
+        print("5.exit")
 
-          choice=input("choose an option(1-5):")
-          if choice =='1':
+        choice=input("choose an option(1-5):")
+        if choice =='1':
             add_contact()
-          elif choice =='2':
+        elif choice =='2':
             view_contacts()
-          elif choice =='3':
-            search_contact()
-          elif choice =='4':
+        elif choice =='3':
+            search_contacts()
+        elif choice =='4':
             delete_contact()
-          elif choice =='5':
+        elif choice =='5':
             print("goodbye!")
-          else:
+        else:
             print("invalid option. try again.\n")
 menu()           
