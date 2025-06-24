66f3c0b8bd8e6575b77ea70a107d6e9f3a624d8a
harreshP <btechece230999@smvec.ac.in>
2025-06-24 21:54:40 +0530

    short_todo.py

--- short_todo.py
+++ short_todo.py
@@ -1,4 +1,27 @@
 tasks=[]
-while true:
-  print("\n1.View 2.Add 3.Update 4.Delete 5.Exit")
-  ch=input("choose:")
+while True:
+    print("\n To-Do List:")
+    for i, task in enumerate(tasks, 1):
+        print(f"{i}. {task}")
+    print("\n Options:")      
+    print("1. Add Tasks")
+    print("2. Remove Task")
+    print("3. Exit")
+
+    choice = input("Choosse an option:")
+
+    if choice == "1":
+        task = input("Enter task:")
+        tasks.append(task)
+    elif choice == "2":
+       try:
+           task_num = int(input("enter task number to remove:"))
+           tasks.pop(task_num -1)
+       except:
+           print("Invalid number")
+    elif choice =="3":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid option.")
+
