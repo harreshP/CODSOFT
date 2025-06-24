30ef55ed43a4499ffc3630cc415bb6768eea8454
harreshP <btechece230999@smvec.ac.in>
2025-06-24 23:16:36 +0530

    Create #simple_contact_book.py

---
+++ #simple_contact_book.py
@@ -0,0 +1,50 @@
+contacts={}
+def add_contact():
+  name=input("Enter name:")
+  phone=inpu("Enter phone number:")
+  contacts[name]=phone
+  print("Contact added!\n")
+def view_contacts():
+  if not contacts:
+    print("no contacts saved.\n")
+  else:
+    print("\ncontact List:")
+    for name,phone in contacts.items():
+      print(f"{name}:{phone}")
+      print()
+def search_contacts():
+  name=inputs("Enter name to search:")
+  if name in contacts:
+    print(f"{name}'s number is {contacts[name]}\n")
+  else:
+    print("contact not found.\n")
+def delete_contact():
+  name=inout("Enter name to delete:")
+  if name in contacts:
+    del contacts[name]
+    print("contact deleted.\n")
+  else:
+    print("contact not found.\n")
+def menu():
+  while true:
+    print("==contact book===')
+          print("1.Add contact")
+          print("2.view contact")
+          print("3.search contact")
+          print("4.delete contact")
+          print("5.exit")
+
+          choice=input("choose an option(1-5):")
+          if choice =='1':
+            add_contact()
+          elif choice =='2':
+            view_contacts()
+          elif choice =='3':
+            search_contact()
+          elif choice =='4':
+            delete_contact()
+          elif choice =='5':
+            print("goodbye!")
+          else:
+            print("invalid option. try again.\n")
+menu()           
